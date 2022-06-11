2edd444f142b11ebfd6680ce3a822b70e66ddb06
‘cyk’ <935765353@qq.com>
2022-06-11 11:30:46 +0800

    项目初版

--- assets/scripts/modules/test/Test1.ts
+++ assets/scripts/modules/test/Test1.ts
@@ -1,5 +1,3 @@
-import { TickMgr } from "../../framework/mgr/TickMgr";
-
 export class Test1 {
     constructor() {
         if (this['ctor']) this['ctor']();
--- assets/scripts/modules/test/Test2.ts
+++ assets/scripts/modules/test/Test2.ts
@@ -1,12 +1,11 @@
 import { Test1 } from "./Test1";
-
 export class Test2 extends Test1 {
     public aa: number;
     private ctor() {
         let self = this;
         self.aa = 55;
         setTimeout(function () {
-            console.log(self.aa); 
+            console.log('self.aa: ' + self.aa); 
         }, 300)
     }
 
